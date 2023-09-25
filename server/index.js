ce065806ce5f02b25119dfd5d4805d86a3f7e232
Nitish <nrk1722@gmail.com>
2023-09-26 01:17:24 +0530

    Update index.js 2

--- server/index.js
+++ server/index.js
@@ -27,7 +27,55 @@ const server = app.listen(port, () => {});
 
 const io = socket(server, {
   cors: {
-    origin: "*",
+    origin: "require("dotenv").config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const express = require("express")
+const userRoutes = require('./routes/userRoutes')
+const messageRoutes=require('./routes/messageRoutes');
+const socket  = require("socket.io");
+const app = express()
+
+app.use(express.static('./public'));
+
+app.use(cors())
+app.use(express.json());
+app.use('/api/auth', userRoutes)
+app.use('/api/msg',messageRoutes)
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then(() => {
+    
+}).catch((err) => {
+    console.log("first")
+
+})
+const port = process.env.PORT || 3001;
+const server=app.listen(port, ()=>{})
+
+const io =socket(server,{
+    cors: {
+        origin: "https://chattingapplication.onrender.com",
+      Credential:true
+        
+    }
+}
+)
+global.onlineUsers = new Map();
+io.on('connection', (socket) => {
+    global.chatSocket = socket;
+    socket.on('add-user', (userId) => {
+        onlineUsers.set(userId, socket.id)
+    })
+    socket.on('send-msg', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to)
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('recv-msg',data.msg)
+        }
+        
+    })
+})",
     
   },
 });
